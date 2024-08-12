7915fde59570ec4200119ed4985a956226ebbefc
DevBarnwal <dev.bharat.03@gmail.com>
2024-08-12 23:28:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_KEY = "1d3a0eefa97b499d8fbc4ee93eeb40b7";
+const API_KEY = "3190a500c4c84952bb3e1603b90d0aaa";
 const url = "https://newsapi.org/v2/everything?q=";
 
 window.addEventListener("load", () => fetchNews("India"));
